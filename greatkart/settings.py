5b3a6a0a22c74910f6bcae67d4d9acea32b9093d
Bruno Lucas Dos Santos <bls2@aluno.ifal.edu.br>
2022-10-15 00:10:23 -0300

    1234

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 #testando comandos git
 #testando comandos git denovo
 #testando comandos git denovo 3
+#123432d
 
 from pathlib import Path
 from decouple import config
